db682093925ade28c6954661e69c45d186672bf7
juri.castellani <juri.castellani@accenture.com>
2017-12-25 11:37:35 +0100

    Void test method and test data structure have been created.

---
+++ JavaKata/src/test/java/com/juricastellani/javakata/karateChop/KarateChopTest.java
@@ -0,0 +1,44 @@
+package com.juricastellani.javakata.karateChop;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class KarateChopTest {
+    Collection<Integer> integerList;
+
+    @Before
+    public void init() throws Exception {
+        integerList = new ArrayList<Integer>() {{
+            add(1);
+            add(4);
+            add(5);
+            add(7);
+            add(10);
+            add(12);
+            add(13);
+            add(16);
+            add(18);
+            add(19);
+            add(20);
+            add(21);
+            add(24);
+        }};
+
+    }
+
+    @After
+    public void close() throws Exception {
+        integerList = null;
+    }
+
+    @Test
+    public void binarySearchTest() throws Exception {
+
+    }
+
+}
