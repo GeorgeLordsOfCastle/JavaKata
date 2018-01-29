da5735c79bca82582a6c8dcc69b31e670f95f9bc
juri.castellani <juri.castellani@accenture.com>
2018-01-29 03:50:47 +0100

    Void binary search kata related classes have been added.

---
+++ JavaKata/src/main/java/com/juricastellani/javakata/n2KarateChop/BinarySearch.java
@@ -0,0 +1,4 @@
+package com.juricastellani.javakata.n2KarateChop;
+
+public class BinarySearch {
+}
---
+++ JavaKata/src/test/java/com/juricastellani/javakata/n2KarateChop/BinarySearchTest.java
@@ -0,0 +1,24 @@
+package com.juricastellani.javakata.n2KarateChop;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinarySearchTest {
+    private BinarySearch binarySearch;
+
+    @Before
+    public void init() throws Exception {
+
+    }
+
+    @After
+    public void close() throws Exception {
+
+    }
+
+    @Test
+    public void searchTest() throws Exception {
+
+    }
+}
