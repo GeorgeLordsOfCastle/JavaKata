1805b23c56b27c9800b4972b097e4aae3ca26b91
juri.castellani <juri.castellani@accenture.com>
2018-01-29 03:58:13 +0100

    Binary search first test has been verified.

--- JavaKata/src/main/java/com/juricastellani/javakata/n2KarateChop/BinarySearch.java
+++ JavaKata/src/main/java/com/juricastellani/javakata/n2KarateChop/BinarySearch.java
@@ -1,4 +1,12 @@
 package com.juricastellani.javakata.n2KarateChop;
 
+import java.util.Arrays;
+
 public class BinarySearch {
+    public int chop(int element, int[] arrayOfInteger) {
+        if(!Arrays.asList(arrayOfInteger).contains(element)) {
+            return -1;
+        }
+        return 0;
+    }
 }
--- JavaKata/src/test/java/com/juricastellani/javakata/n2KarateChop/BinarySearchTest.java
+++ JavaKata/src/test/java/com/juricastellani/javakata/n2KarateChop/BinarySearchTest.java
@@ -4,21 +4,24 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class BinarySearchTest {
     private BinarySearch binarySearch;
 
     @Before
     public void init() throws Exception {
-
+        binarySearch = new BinarySearch();
     }
 
     @After
     public void close() throws Exception {
-
+        binarySearch = null;
     }
 
     @Test
     public void searchTest() throws Exception {
-
+        assertThat(binarySearch.chop(3, new int[]{}), is(-1));
     }
 }
